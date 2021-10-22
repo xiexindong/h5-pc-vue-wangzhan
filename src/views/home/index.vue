67d76336992505b5aa0c36798bd5199a617d0fa4
xindong.xie <779788582@qq.com>
2021-10-22 18:02:19 +0800

    first commit

---
+++ src/views/home/index.vue
@@ -0,0 +1,24 @@
+<!-- home -->
+<template>
+  <div class="index-container">
+      <div class="animated fadeInUp">zhaongguo</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+
+  computed: {},
+
+  mounted() { },
+
+  methods: {}
+}
+</script>
+<style lang="scss" scoped>
+</style>
